5a8d554040be91beb9952066ad3be9fef4be60a3
Abenx <5195693+rushairer@users.noreply.github.com>
2021-10-12 16:43:48 +0800

    [Init]

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # RoundedCorners
 
-A description of this package.
+RoundedCorners for SwiftUI.
+
+
+## Overview
+
+```swift
+
+struct RoundedCornersDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        Color.red.cornerRadius(tl: 14, tr: 14, bl: 30, br: 30)
+            .frame(width: 60, height: 60)
+    }
+}
+
+```
--- Tests/RoundedCornersTests/RoundedCornersTests.swift
+++ Tests/RoundedCornersTests/RoundedCornersTests.swift
@@ -1,11 +1,13 @@
 import XCTest
+import SwiftUI
 @testable import RoundedCorners
 
 final class RoundedCornersTests: XCTestCase {
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(RoundedCorners().text, "Hello, World!")
+        XCTAssertEqual(RoundedCorners().tl, 0.0)
     }
 }
